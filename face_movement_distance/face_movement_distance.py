c5d7de0b489f0d98bcab6e0792add5e485bc4b1c
Lee-DaeSeong <dleotjd510@naver.com>
2022-01-16 01:17:06 +0900

    얼굴 움직임(거리) 계산

---
+++ face_movement_distance/face_movement_distance.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+# https://icodebroker.tistory.com/5323
+
+def get_movement_distance(cap, frame_interval):
+    cur_frame = 1.0
+    last_frame = cap.get(cv2.CAP_PROP_FRAME_COUNT) - 30
+    while cur_frame <= last_frame:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, cur_frame)
+        ret, frame = cap.read()
+        print(cap.get(cv2.CAP_PROP_POS_FRAMES))
+        frame = cv2.flip(frame, 1)  # 좌우 대칭
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(gray, 1.05, 5)
+
+        if len(faces):
+            for (x, y, w, h) in faces:
+                p = str(x) + ', ' + str(y)
+
+        k = cv2.waitKey(30) & 0xff
+        if k == 27 or k == ord('q'):  # Esc 키를 누르면 종료
+            break
+        cur_frame += frame_interval
+    cap.release()
+    cv2.destroyAllWindows()
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture('../data/sample.avi')
+frame_interval = 100
+get_movement_distance(cap, frame_interval)
+
+
