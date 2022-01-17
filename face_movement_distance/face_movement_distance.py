952d7d1a562ef0977e1aaeeccbaed028eb7e4aa2
Lee-DaeSeong <dleotjd510@naver.com>
2022-01-18 01:48:02 +0900

    0118

--- face_movement_distance/face_movement_distance.py
+++ face_movement_distance/face_movement_distance.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-# https://icodebroker.tistory.com/5323
+import csv
 
 def get_movement_distance(cap, frame_interval):
     cur_frame = 1.0
@@ -8,7 +8,6 @@ def get_movement_distance(cap, frame_interval):
     while cur_frame <= last_frame:
         cap.set(cv2.CAP_PROP_POS_FRAMES, cur_frame)
         ret, frame = cap.read()
-        print(cap.get(cv2.CAP_PROP_POS_FRAMES))
         frame = cv2.flip(frame, 1)  # 좌우 대칭
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -17,17 +16,20 @@ def get_movement_distance(cap, frame_interval):
         if len(faces):
             for (x, y, w, h) in faces:
                 p = str(x) + ', ' + str(y)
-
+                print(p)
         k = cv2.waitKey(30) & 0xff
         if k == 27 or k == ord('q'):  # Esc 키를 누르면 종료
             break
         cur_frame += frame_interval
-    cap.release()
-    cv2.destroyAllWindows()
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-cap = cv2.VideoCapture('../data/sample.avi')
+base = '../data/'
+file_name='sample.avi'
+cap = cv2.VideoCapture(base + file_name)
 frame_interval = 100
+f = open(base+file_name, 'w', encoding='utf-8-sig')
+writer = csv.writer(f)
+
 get_movement_distance(cap, frame_interval)
 
 
